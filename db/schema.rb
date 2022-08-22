285f9cf18369d6de234ed3c5788edbd9ac771e79
Sid Mann <sjmann2@outlook.com>
2022-08-22 17:48:46 -0500

    Add author model and migrate database with books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_223846) do
+ActiveRecord::Schema.define(version: 2022_08_22_224255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2022_08_22_223846) do
     t.boolean "in_print"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
