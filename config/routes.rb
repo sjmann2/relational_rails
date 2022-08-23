a7892f4aea8006c8d25415d1aae0a0e958419e73
Sid Mann <sjmann2@outlook.com>
2022-08-22 20:53:41 -0500

    Add author books index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
   get '/authors', to: 'authors#index'
+  get 'authors/:id', to: 'authors#show'
+  get 'authors/:author_id/books', to: 'author_books#index'
 end
