98348676b5f793cb14a10058446b0413927ff463
Sid Mann <sjmann2@outlook.com>
2022-08-24 21:23:19 -0500

    Add authors #create and #new

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,12 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def new  
+  end
+
+  def create
+    author = Author.create!(name: params[:name], currently_alive: params[:currently_alive], age_when_first_published: params[:age_when_first_published])
+    redirect_to '/authors'
+  end
 end
