30748bef89469fc27092ccc66d1fea3b58a66a2e
Sid Mann <sjmann2@outlook.com>
2022-08-25 16:35:35 -0500

    Add update book feature and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post '/authors', to: 'authors#create'
   get '/authors/:id/edit', to: 'authors#edit'
   patch '/authors/:id', to: 'authors#update'
-  get 'authors/:author_id/books/new', to: 'author_books#new'
+  get '/authors/:author_id/books/new', to: 'author_books#new'
   post '/authors/:author_id/books', to: 'author_books#create'
+  get '/books/:id/edit', to: 'books#edit'
+  patch 'books/:id', to: 'books#update'
 end
