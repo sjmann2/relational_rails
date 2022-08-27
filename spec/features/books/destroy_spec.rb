e2a38abfb13c86248ffb084632be862efe5dc937
Sid Mann <sjmann2@outlook.com>
2022-08-27 11:14:18 -0500

    Add delete book feature from book index

--- spec/features/books/destroy_spec.rb
+++ spec/features/books/destroy_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'destroying a book' do
-  before :each do
-    @author = Author.create!(name: "Toni Morrison", currently_alive: false, age_when_first_published: 39)
-    @book = @author.books.create!(name: "The Bluest Eye", length: 404, in_print: true)
+  describe 'deleting a book from the show page' do
+    before :each do
+      @author = Author.create!(name: "Toni Morrison", currently_alive: false, age_when_first_published: 39)
+      @book = @author.books.create!(name: "The Bluest Eye", length: 404, in_print: true)
+    end
+    
+    it 'can delete the book from the show page' do
+      visit "/books/#{@book.id}"
+
+      click_link("Delete Book")
+
+      expect(current_path).to eq("/books")
+      expect(page).to_not have_content("The Bluest Eye")
+    end
   end
-  
-  it 'can delete the book from the show page' do
-    visit "/books/#{@book.id}"
 
-    click_link("Delete Book")
+  describe 'deleting a book from the index page' do
+    before :each do
+      @author = Author.create!(name: "Toni Morrison", currently_alive: false, age_when_first_published: 39)
+      @book = @author.books.create!(name: "The Bluest Eye", length: 404, in_print: true)
+    end
 
-    expect(current_path).to eq("/books")
-    expect(page).to_not have_content("The Bluest Eye")
+    it 'can delete a book from the index page' do
+      visit "/books"
+
+      click_link("Delete #{@book.name}")
+
+      expect(current_path).to eq("/books")
+      expect(page).to_not have_content("Toni Morrison")
+    end
   end
 end
