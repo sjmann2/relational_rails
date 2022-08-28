b4db295b256cfc6fffad4264adc8841bf828b23e
Sid Mann <sjmann2@outlook.com>
2022-08-27 20:43:24 -0500

    Add test for sorting authors by book count

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe 'the authors index page' do
 
     it 'displays a list of all authors' do
       visit "/authors"
-
-      expect(page).to have_content(@jane_austen.name)
-      expect(page).to have_content(@leslie_feinberg.name)
-      expect(page).to have_content(@carmen_maria_machado.name)
+      
+      expect(page).to have_content("Jane Austen")
+      expect(page).to have_content("Leslie Feinberg")
+      expect(page).to have_content("Carmen Maria Machado")
     end
 
     it 'displays created at timestamp for each author' do
@@ -74,4 +74,54 @@ RSpec.describe 'the authors index page' do
       end
     end
   end
+
+  describe 'I see a link to sort authors by the number of books they have' do
+    before :each do
+      @jane_austen = Author.create!(name: "Jane Austen", currently_alive: false, age_when_first_published: 21)
+      @leslie_feinberg = Author.create!(name: "Leslie Feinberg", currently_alive: false, age_when_first_published: 40)
+      @carmen_maria_machado = Author.create!(name: "Carmen Maria Machado", currently_alive: true, age_when_first_published: 31) 
+      @book_1 = @carmen_maria_machado.books.create!(name: "Her Body and Other Parties", length: 380, in_print: true)
+      @book_2 = @carmen_maria_machado.books.create!(name: "In the Dream House", length: 321, in_print: true)
+      @book_3 = @carmen_maria_machado.books.create!(name: "The Low Low Woods", length: 189, in_print: true)
+      @book_3 = @jane_austen.books.create!(name: "Sense and Sensibility", length: 402, in_print: true)
+      @book_4 = @jane_austen.books.create!(name: "Pride and Prejudice", length: 387, in_print: true)
+      @book_5 = @leslie_feinberg.books.create!(name: "Stone Butch Blues", length: 300, in_print: false)
+    end
+
+    it 'displays a link that sorts authors by number of books' do
+      visit "/authors"
+
+      click_link("Sort authors by number of books")
+
+      expect(current_path).to eq("/authors")
+    end
+
+    it 'sorts authors by number of books' do
+      visit "/authors"
+
+      click_link("Sort authors by number of books")
+
+      expect("Carmen Maria Machado").to appear_before("Jane Austen")
+      expect("Jane Austen").to appear_before("Leslie Feinberg")
+    end
+
+    it 'shows the count of books next to each author' do
+      visit "/authors"
+
+      click_link("Sort authors by number of books")
+
+      expect(page).to have_content("3")
+      expect(page).to have_content("2")
+      expect(page).to have_content("1")
+    end
+  end
 end
+# Sort Parents by Number of Children 
+
+# As a visitor
+# When I visit the Parents Index Page
+# Then I see a link to sort parents by the number of `child_table_name` they have
+# When I click on the link
+# I'm taken back to the Parent Index Page where I see all of the parents
+# in order of their count of `child_table_name` (highest to lowest) And, 
+# I see the number of children next to each parent name
